69e89cadb6d4385feb2faf55d08ffc45220f3b59
kalo <kaolin1983@abv.bg>
2017-07-29 20:20:00 +0300

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = "kaloyan";
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double area = a * b;
+            Console.WriteLine(area); 
+
+
+        }
+       
+    }
+}
